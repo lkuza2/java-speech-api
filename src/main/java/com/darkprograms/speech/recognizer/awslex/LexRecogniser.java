0f77d0cb80a01c5649ad4043df7f42cfe51e688e
Nicholas Albion <nalbion@yahoo.com>
2017-07-13 21:57:13 +1000

    more ways of providing sessionAttributes, application provides LexRuntime

--- src/main/java/com/darkprograms/speech/recognizer/awslex/LexRecogniser.java
+++ src/main/java/com/darkprograms/speech/recognizer/awslex/LexRecogniser.java
@@ -1,26 +1,107 @@
 package com.darkprograms.speech.recognizer.awslex;
 
 import com.amazonaws.services.lexruntime.AmazonLexRuntime;
-import com.amazonaws.services.lexruntime.AmazonLexRuntimeClientBuilder;
 import com.amazonaws.services.lexruntime.model.PostContentRequest;
 import com.amazonaws.services.lexruntime.model.PostContentResult;
-import com.amazonaws.services.lexruntime.model.PostTextRequest;
 import com.darkprograms.speech.recognizer.RecognitionResult;
+import org.json.JSONObject;
 
 import javax.sound.sampled.AudioInputStream;
+import java.util.Base64;
 import java.util.Map;
 
-public class LexRecogniser {
-    private String userId;
-    private AmazonLexRuntime lex;
+/**
+ * example:
+ * <pre>
+ LexRecogniser lex = new LexRecogniser(AmazonLexRuntimeClientBuilder.defaultClient(), "MyLexBot", "PROD", "auser");
+ MicrophoneAnalyzer mic = new MicrophoneAnalyzer(null);
+ VoiceActivityDetector vad = new VoiceActivityDetector();
 
-    LexRecogniser(String userId) {
+ vad.detectVoiceActivity(mic, audioInputStream -> {
+    PostContentResult result = lex.getRecognizedDataForStream(audioInputStream, myApp.getSessionAttributes()).getResult();
+    System.out.println(result.message);
+ });
+ * </pre>
+ */
+public class LexRecogniser {
+    private AmazonLexRuntime lex;
+    private String botName;
+    private String botAlias;
+    private String userId;
+
+    public LexRecogniser(AmazonLexRuntime lex, String botName, String botAlias, String userId) {
+        this.lex = lex;
+        this.botName = botName;
+        this.botAlias = botAlias;
         this.userId = userId;
-        lex = AmazonLexRuntimeClientBuilder.standard()
-//                .withClientConfiguration()
-//                .withCredentials()
-//                .withRegion()
-                .build();
+    }
+
+    public RecognitionResult getRecognizedDataForStream(AudioInputStream stream, Map<String, String> sessionAttributes) {
+        String json;
+        if (sessionAttributes == null || sessionAttributes.isEmpty()) {
+            json = null;
+        } else {
+            StringBuilder str = null;
+
+            for (Map.Entry<String, String> entry : sessionAttributes.entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+
+                if (str == null) {
+                    str = new StringBuilder("{");
+                } else {
+                    str.append(",");
+                }
+
+                str.append("\"").append(key).append("\":");
+                if (value == null) {
+                    str.append("null");
+                } else {
+                    str.append(JSONObject.quote(value));
+                }
+            }
+
+            json = str.append("}").toString();
+        }
+
+        return getRecognizedDataForStream(stream, new String(Base64.getEncoder().encode(json.getBytes())));
+    }
+
+    public RecognitionResult getRecognizedDataForStreamWithObjects(AudioInputStream stream, Map<String, Object> sessionAttributes) {
+        String json;
+        if (sessionAttributes == null || sessionAttributes.isEmpty()) {
+            json = null;
+        } else {
+            StringBuilder str = null;
+
+            for (Map.Entry<String, Object> entry : sessionAttributes.entrySet()) {
+                String key = entry.getKey();
+                Object value = entry.getValue();
+
+                if (str == null) {
+                    str = new StringBuilder("{");
+                } else {
+                    str.append(",");
+                }
+
+                str.append("\"").append(key).append("\":");
+//                if (value == null) {
+//                    str.append("null");
+//                } else if (value instanceof Number) {
+//                    str.append(JSONObject.numberToString((Number)value));
+//                } else if (value instanceof Boolean) {
+//                    str.append(((Boolean)value).toString());
+//                } else if (value instanceof String) {
+//                    str.append("\"").append((String)value).append("\"");
+//                } else {
+                    str.append(JSONObject.valueToString(value));
+//                }
+            }
+
+            json = str.append("}").toString();
+        }
+
+        return getRecognizedDataForStream(stream, new String(Base64.getEncoder().encode(json.getBytes())));
     }
 
     /**
@@ -30,8 +111,8 @@ public class LexRecogniser {
      */
     public RecognitionResult getRecognizedDataForStream(AudioInputStream stream, String sessionAttributes) {
         PostContentRequest request = new PostContentRequest()
-                .withBotName("idear")
-                .withBotAlias("PROD")
+                .withBotName(botName)
+                .withBotAlias(botAlias)
                 .withInputStream(stream)
                 .withUserId(userId)
                 .withSessionAttributes(sessionAttributes);
