49d0c358c7be4be1024d8e774d03650bd0ad255b
GOXR3PLUS <crAlexcomp@gmail.com>
2017-08-25 23:19:34 +0300

    Moved enum to outer class

--- src/main/java/com/darkprograms/speech/recognizer/Recognizer.java
+++ src/main/java/com/darkprograms/speech/recognizer/Recognizer.java
@@ -15,103 +15,6 @@ import org.json.*;
  **************************************************************/
 public class Recognizer {
 
-    public enum Languages{
-		AUTO_DETECT("auto"),//tells Google to auto-detect the language
-		ARABIC_JORDAN("ar-JO"),
-		ARABIC_LEBANON("ar-LB"),
-		ARABIC_QATAR("ar-QA"),
-		ARABIC_UAE("ar-AE"),
-		ARABIC_MOROCCO("ar-MA"),
-		ARABIC_IRAQ("ar-IQ"),
-		ARABIC_ALGERIA("ar-DZ"),
-		ARABIC_BAHRAIN("ar-BH"),
-		ARABIC_LYBIA("ar-LY"),
-		ARABIC_OMAN("ar-OM"),
-		ARABIC_SAUDI_ARABIA("ar-SA"),
-		ARABIC_TUNISIA("ar-TN"),
-		ARABIC_YEMEN("ar-YE"),
-		BASQUE("eu"),
-		CATALAN("ca"),
-		CZECH("cs"),
-		DUTCH("nl-NL"),
-		ENGLISH_AUSTRALIA("en-AU"),
-		ENGLISH_CANADA("en-CA"),
-		ENGLISH_INDIA("en-IN"),
-		ENGLISH_NEW_ZEALAND("en-NZ"),
-		ENGLISH_SOUTH_AFRICA("en-ZA"),
-		ENGLISH_UK("en-GB"),
-		ENGLISH_US("en-US"),
-		FINNISH("fi"),
-		FRENCH("fr-FR"),
-		GALICIAN("gl"),
-		GERMAN("de-DE"),
-		HEBREW("he"),
-		HUNGARIAN("hu"),
-		ICELANDIC("is"),
-		ITALIAN("it-IT"),
-		INDONESIAN("id"),
-		JAPANESE("ja"),
-		KOREAN("ko"),
-		LATIN("la"),
-		CHINESE_SIMPLIFIED("zh-CN"),
-		CHINESE_TRANDITIONAL("zh-TW"),
-		CHINESE_HONGKONG("zh-HK"),
-		CHINESE_CANTONESE("zh-yue"),
-		MALAYSIAN("ms-MY"),
-		NORWEGIAN("no-NO"),
-		POLISH("pl"),
-		PIG_LATIN("xx-piglatin"),
-		PORTUGUESE("pt-PT"),
-		PORTUGUESE_BRASIL("pt-BR"),
-		ROMANIAN("ro-RO"),
-		RUSSIAN("ru"),
-		SERBIAN("sr-SP"),
-		SLOVAK("sk"),
-		SPANISH_ARGENTINA("es-AR"),
-		SPANISH_BOLIVIA("es-BO"),
-		SPANISH_CHILE("es-CL"),
-		SPANISH_COLOMBIA("es-CO"),
-		SPANISH_COSTA_RICA("es-CR"),
-		SPANISH_DOMINICAN_REPUBLIC("es-DO"),
-		SPANISH_ECUADOR("es-EC"),
-		SPANISH_EL_SALVADOR("es-SV"),
-		SPANISH_GUATEMALA("es-GT"),
-		SPANISH_HONDURAS("es-HN"),
-		SPANISH_MEXICO("es-MX"),
-		SPANISH_NICARAGUA("es-NI"),
-		SPANISH_PANAMA("es-PA"),
-		SPANISH_PARAGUAY("es-PY"),
-		SPANISH_PERU("es-PE"),
-		SPANISH_PUERTO_RICO("es-PR"),
-		SPANISH_SPAIN("es-ES"),
-		SPANISH_US("es-US"),
-		SPANISH_URUGUAY("es-UY"),
-		SPANISH_VENEZUELA("es-VE"),
-		SWEDISH("sv-SE"),
-		TURKISH("tr"),
-		ZULU("zu");
-	    
-		//TODO Clean Up JavaDoc for Overloaded Methods using @link
-		
-	    /**
-	     *Stores the LanguageCode
-	     */
-	    private final String languageCode;
-	    
-	    /**
-	     *Constructor
-	     */
-	    private Languages(final String languageCode){
-	    	this.languageCode = languageCode;
-	    }
-
-	    public String toString(){
-	    	return languageCode;
-	    }
-	      
-	}
-
-
     /**
      * URL to POST audio data and retrieve results
      */
@@ -138,7 +41,7 @@ public class Recognizer {
      * @param apikey
      */
      public Recognizer(Languages language, String apikey){
-     	this.language = language.languageCode;
+     	this.language = language.getLanguageCode();
         this.apikey = apikey;
      }
     
@@ -171,7 +74,7 @@ public class Recognizer {
      * @param apikey
      */
      public Recognizer(Languages language, boolean profanityFilter, String apikey){
-     	this.language = language.languageCode;
+     	this.language = language.getLanguageCode();
      	this.profanityFilter = profanityFilter;
         this.apikey = apikey;
      }
@@ -183,7 +86,7 @@ public class Recognizer {
      * @param language
      */
     public void setLanguage(Languages language) {
-        this.language = language.languageCode;
+        this.language = language.getLanguageCode();
     }
     
     /**Language code.  This language code must match the language of the speech to be recognized. ex. en-US ru-RU
